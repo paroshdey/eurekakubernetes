2c7215fcc50bc4c5b0858982a49bbeeace3364d1
Parosh Dey <parosde1@in.ibm.com>
2020-02-03 20:32:40 +0530

    initial commit

---
+++ EurekaClient1/src/main/java/com/amdocs/EurekaClient1/EurekaClient1Application.java
@@ -0,0 +1,27 @@
+package com.amdocs.EurekaClient1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClient1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClient1Application.class, args);
+	}
+
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate getResttemplate() {
+		 return new RestTemplate();
+
+	}
+	
+}
